8f1e1288f6916ae45decc85b2a98824bc5f44d9a
MCHectorious <MCHectorious@gmail.com>
2018-05-05 21:43:02 +0100

    Final Version

--- app/src/main/java/com/hector/csprojectprogramc/Activities/CourseListScreen.java
+++ app/src/main/java/com/hector/csprojectprogramc/Activities/CourseListScreen.java
@@ -1,6 +1,5 @@
 package com.hector.csprojectprogramc.Activities;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
